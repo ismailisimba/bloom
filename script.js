b5f21637e4f2f7e0763bcc7e5401d36863873f4b
Ismail Amir <Ismaili@TIM.ISTAFRICA.COM>
2020-10-26 15:01:05 +0300

    added post with complex objects appended on the go

--- script.js
+++ script.js
@@ -6,10 +6,18 @@
         let tempDiv2 = document.createElement("p");
         tempDiv2.innerHTML = title;
 
+        let homeButt1 = document.getElementById("homepageformbutton");
 
-        let filterOne = {"Hello":"There!",
-                        "General":"Palpllatine?",};
-        filterOne = JSON.stringify(filterOne);
+        let reqString = 'https://script.google.com/macros/s/AKfycbz4bqgr0lOgvfhraAjEN6_KVVd68D-Jfb1Qnj4pk3V2VSp802I/exec'+'?'+"filterOne=";
+
+
+        let filterOne = {"Hello":"Iam",
+                        "filter":"one",};
+
+        let bodyData = {"Hello":"Iam",
+                        "Body":"Data",}
+
+        
 
 /*perm variable*/
 
@@ -20,28 +28,30 @@ let localVar = {"one":"one"};
 function mainFunc() {
 
     testfunc();
-    fetchInfoWithFilter (filterOne);
+  // fetchInfoWithFilter (filterOne);
+   
+ // filterOne = JSON.stringify(filterOne);
+  // postData(reqString+filterOne,bodyData);
+
+   homeButt1.addEventListener("click", function(){homepageFormButton();})
 
   };
 
   mainFunc();
 
 
-  function testfunc () {
-
-  
-     
+function testfunc () {     
      tempDiv.id = "testdiv3000";
      tempDiv.appendChild(tempDiv2);
      tempStyle.innerHTML = "#testdiv3000 { display: block; position: absolute; top: 150px; left: 25%; width: 50%; height: 350px; color: white; background-color: red; z-index: 20;}"
  
      window.document.body.appendChild(tempStyle);
-     window.document.body.appendChild(tempDiv);
-     
+     window.document.body.appendChild(tempDiv);    
   }
 
 
-   async function fetchInfoWithFilter (filterOne) {
+async function fetchInfoWithFilter (filterOne) {
+    filterOne = JSON.stringify(filterOne);
     const myInit = {
         method: "GET",
         mode: "cors",
@@ -53,7 +63,7 @@ function mainFunc() {
         redirect: 'follow'
       };
 
-    var myRequest = new Request('https://script.google.com/macros/s/AKfycbz4bqgr0lOgvfhraAjEN6_KVVd68D-Jfb1Qnj4pk3V2VSp802I/exec'+'?'+"filterOne="+filterOne);
+    var myRequest = new Request(reqString+filterOne);
     
 
          
@@ -87,10 +97,69 @@ function mainFunc() {
 
           localVar = returnVal; 
 
-          tempDiv2.innerHTML = Object.keys(localVar);
-
-        
-         
-        
-        
+          tempDiv2.innerHTML = Object.keys(localVar);   
 };
+
+
+
+
+async function postData(url = '', data = {}) {
+   
+    // Default options are marked with *
+    const cloudObject = await fetch(url, {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      mode: 'cors', // no-cors, *cors, same-origin
+      cache: 'default', // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: 'omit', // include, *same-origin, omit
+      headers: {
+        //'Content-Type': 'text/txt'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow', // manual, *follow, error
+      referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+      body: JSON.stringify(data) // body data type must match "Content-Type" header
+    }).then(function(response){
+        
+        let cloudy =  response.text();
+        
+        
+        
+        
+
+        return cloudy;
+    }).then(function(myBlob) {
+
+        var cloudObjecty = JSON.parse(myBlob);
+            
+          
+            return cloudObjecty;
+
+    });
+     
+    
+    
+
+    tempDiv2.innerHTML = Object.keys(cloudObject)[2];
+  }
+
+
+
+  function homepageFormButton () {
+
+      let tempNameValue = document.getElementById("hometextname").value;
+      let tempEmailValue = document.getElementById("homeemailname").value;
+
+      let formObject = {"name": tempNameValue,
+                         "email": tempEmailValue};
+
+     // tempDiv2.innerHTML = tempNameValue+"<br>"+tempEmailValue;
+
+     bodyData["forms"] = {"form1":[{"form-name":"homepageform"}]};
+     bodyData["forms"]["form1"].push(formObject);
+
+   filterOne = JSON.stringify(filterOne);
+  postData(reqString+filterOne,bodyData);
+
+
+      
+  }
